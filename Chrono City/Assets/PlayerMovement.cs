8868e6f6d5a06953612e83371ef1da20a7a9549f
Allaezhya Vanness <allaezhya@gmail.com>
2024-04-25 11:02:46 +0700

    Run Implementation

--- Chrono City/Assets/PlayerMovement.cs
+++ Chrono City/Assets/PlayerMovement.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rbody;
+
+    private float horizontalMove;
+
+    [SerializeField] private float speed = 10f;
+
+    private void Awake()
+    {
+        _rbody = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMove = Input.GetAxis("Horizontal");
+        _rbody.velocity = new Vector2(horizontalMove * speed, _rbody.velocity.y);
     }
 }
