2317d3a637de8317c17bb8cc34ac34e0f818ec37
Allaezhya Vanness <allaezhya@gmail.com>
2024-04-30 09:57:32 +0700

    Add Comment to Help Documentation and Double Jump Feature

--- Chrono City/Assets/Game Scene/PlayerMovement.cs
+++ Chrono City/Assets/Game Scene/PlayerMovement.cs
@@ -10,31 +10,37 @@ public class Movement : MonoBehaviour
 
     bool isFacingRight = true;
 
+    // Movement Feature
     [Header("Movement")]
     public float speed = 5f;
     float horizontalMovement;
 
+    // Jump & Double Jump Feature 
     [Header("Jumping")]
     public float jumpPower = 11f;
     public int maxJumps = 1;
     int jumpsRemaining;
 
+    // Ground Check Feature
     [Header("GroundCheck")]
     public Transform groundCheckPos;
     public Vector2 groundCheckSize = new Vector2(0.82f, 0.07f);
     public LayerMask groundLayer;
     bool isGrounded;
 
+    // Faster Fall Feature
     [Header("Gravity")]
     public float baseGravity = 2f;
     public float maxFallSpeed = 18f;
     public float fallSpeedMult = 2f;
 
+    // Wall Check Feature
     [Header("WallCheck")]
     public Transform wallCheckPos;
     public Vector2 wallCheckSize = new Vector2(0.05f, 2.65f);
     public LayerMask wallLayer;
 
+    // Wall Slide Feature
     [Header("WallMovement")]
     public float wallSlideSpeed = 2f;
     bool isWallSliding;
@@ -55,7 +61,7 @@ public class Movement : MonoBehaviour
         WallSlide();
     }
 
-    private void Flip()
+    private void Flip() // Flip the character when facing right or left
     {
         if (isFacingRight && horizontalMovement < 0 || !isFacingRight && horizontalMovement > 0)
         {
@@ -66,12 +72,12 @@ public class Movement : MonoBehaviour
         }
     }
 
-    public void Move(InputAction.CallbackContext context)
+    public void Move(InputAction.CallbackContext context) // Input for Movement || Movement Method
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
     }
 
-    public void Jump(InputAction.CallbackContext context)
+    public void Jump(InputAction.CallbackContext context) // Input for Jump || Jump Method & Double Jump Method
     {
         if (jumpsRemaining > 0)
             if (context.performed)
@@ -81,7 +87,7 @@ public class Movement : MonoBehaviour
             }
     }
 
-    private void GroundCheck()
+    private void GroundCheck() // Ground Check Method
     {
         if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
         {
@@ -95,12 +101,12 @@ public class Movement : MonoBehaviour
         
     }
 
-    private bool WallCheck()
+    private bool WallCheck() // Wall Check Method
     {
         return Physics2D.OverlapBox(wallCheckPos.position, wallCheckSize, 0, wallLayer);
     }
 
-    private void Gravity()
+    private void Gravity() // Faster Fall Method
     {
         if (_rbody.velocity.y < 0)
         {
@@ -114,7 +120,7 @@ public class Movement : MonoBehaviour
             
     }
 
-    private void WallSlide()
+    private void WallSlide() // Wall Slide Method
     {
         if (!isGrounded && WallCheck() && horizontalMovement != 0)
         {
@@ -127,7 +133,7 @@ public class Movement : MonoBehaviour
         }
     }
 
-    private void OnDrawGizmos()
+    private void OnDrawGizmos() // Drawing Transparent Cube (Deleted When Game is Builded)
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
